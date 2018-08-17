910fe5edca7103916347b01dc1f167e996325842
Maksym Kramarenko <maksym@wix.com>
2018-08-17 21:00:56 +0300

    add logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducer from '../reducer'
+import logger from '../middleware'
 
-const store = createStore(reducer)
+const store = createStore(reducer, {}, applyMiddleware(logger))
 
 // Just for dev/show purpose
 window.store = store
