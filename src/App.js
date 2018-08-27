ccecab64384276b14cc7492488403853e22a2b12
Maksym Kramarenko <maksym@wix.com>
2018-08-27 19:59:22 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import ArticleList from './components/article-list'
 import UserForm from './components/user-form'
 import Filters from './components/filters'
 import Counter from './components/counter'
+import { Route, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
         <UserForm />
-        <Counter />
-        <Filters />
-        <ArticleList />
+        <ul>
+          <li>
+            <NavLink to={'/counter'} activeStyle={{ color: 'red' }}>
+              counter
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={'/filters'} activeStyle={{ color: 'red' }}>
+              filters
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={'/articles'} activeStyle={{ color: 'red' }}>
+              articles
+            </NavLink>
+          </li>
+        </ul>
+        <Route path="/counter" component={Counter} />
+        <Route path="/filters" component={Filters} />
+        <Route path="/articles" component={ArticleList} />
       </div>
     )
   }
